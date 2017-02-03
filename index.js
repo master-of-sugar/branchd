504347d6ccf81c235d1a104eab46f4384f970b0c
Takahiko Sato <t.sato@mosa-architect.com>
2017-02-03 19:12:37 +0900

    コンソール出力に色をつける

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ const git = require('gitty');
 const repo = git('./');
 
 
+const black   = '\u001b[30m';
+const red     = '\u001b[31m';
+const green   = '\u001b[32m';
+const yellow  = '\u001b[33m';
+const blue    = '\u001b[34m';
+const magenta = '\u001b[35m';
+const cyan    = '\u001b[36m';
+const white   = '\u001b[37m';
+
+const reset   = '\u001b[0m';
+
 repo.getBranches((n,branches) => {
   if(!branches){
     console.log('Branch nothing...');
@@ -20,10 +31,10 @@ repo.getBranches((n,branches) => {
       return ob != 'master' && ob != 'develop';
     })
     .forEach((ob) =>{
-      const ans = readlineSync.question('Delete branch [' + ob + ']?(yes)/n : ');
+      const ans = readlineSync.question(reset + 'Delete branch [' + green + ob + reset + ']?(yes)/n : ');
       if(ans !== 'n' && ans !== 'N'){
         execSync('git branch -D ' + ob,{cwd: repo.path});
-        console.log('Deleted [' + ob + ']');
+        console.log(reset + 'Deleted [' + green + ob + reset + ']');
       }
     });
 });
